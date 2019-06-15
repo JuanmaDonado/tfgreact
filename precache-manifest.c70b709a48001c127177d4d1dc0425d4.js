f1a5162ce79f37e2378e4413859533429887f327
Juanma Donado <juanmadoji@gmail.com>
2019-06-16 01:34:20 +0200

    Updates

---
+++ precache-manifest.c70b709a48001c127177d4d1dc0425d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce557dbdbe80fdd385663e3bfdc1466",
+    "url": "/tfgreact/index.html"
+  },
+  {
+    "revision": "fb0f81626cda0c5f98ac",
+    "url": "/tfgreact/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "e2fd9cce68f57961c9a2",
+    "url": "/tfgreact/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "fb0f81626cda0c5f98ac",
+    "url": "/tfgreact/static/js/2.38db8605.chunk.js"
+  },
+  {
+    "revision": "e2fd9cce68f57961c9a2",
+    "url": "/tfgreact/static/js/main.a245a5f4.chunk.js"
+  },
+  {
+    "revision": "8f4409a23d241efe64ec",
+    "url": "/tfgreact/static/js/runtime~main.7f050bea.js"
+  }
+]);
